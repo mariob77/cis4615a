4f22647010325a43d4dfc44db1106e2cd9baafb2
Michael <69060612+mariob77@users.noreply.github.com>
2021-10-14 20:59:12 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,8 +1,8 @@
 
 if (loginSuccessful) {
-  logger.severe("User login succeeded for: " + username);
+  logger.severe("User login succeeded for: " + sanitizeUser(username));
 } else {
-  logger.severe("User login failed for: " + username);
+  logger.severe("User login failed for: " + sanitizeUser(username));
 }
 
 public String sanitizeUser(String username) {
